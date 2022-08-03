f596cf05ecdde550d0f4bc0553d6977d8ea3f47f
Ciro Mota <ciro.mota@gmail.com>
2022-08-03 15:14:52 -0300

    build: Added datasource for Ubuntu 2204 and regex fix for Oracle Linux.

--- datasource.tf
+++ datasource.tf
@@ -1,31 +1,51 @@
-data "oci_core_images" "ubuntu" {
+data "oci_core_images" "ubuntu20" {
   compartment_id   = local.compartment_id
   operating_system = "Canonical Ubuntu"
+  sort_by          = "TIMECREATED"
+
   filter {
     name   = "display_name"
     values = ["^Canonical-Ubuntu-20.04-([\\.0-9-]+)$"]
     regex  = true
   }
 }
+
+data "oci_core_images" "ubuntu22" {
+  compartment_id   = local.compartment_id
+  operating_system = "Canonical Ubuntu"
+  sort_by          = "TIMECREATED"
+
+  filter {
+    name   = "display_name"
+    values = ["^Canonical-Ubuntu-22.04-([\\.0-9-]+)$"]
+    regex  = true
+  }
+}
+
 data "oci_core_images" "ubuntu_aarch64" {
   compartment_id   = local.compartment_id
   operating_system = "Canonical Ubuntu"
+  sort_by          = "TIMECREATED"
+
   filter {
     name   = "display_name"
     values = ["^Canonical-Ubuntu-20.04-aarch64-([\\.0-9-]+)$"]
     regex  = true
   }
 }
+
 data "oci_core_images" "oraclelinux" {
   compartment_id   = local.compartment_id
   operating_system = "Oracle Linux"
+  sort_by          = "TIMECREATED"
 
   filter {
     name   = "display_name"
-    values = ["^Oracle-Linux-8.5-([\\.0-9-]+)$"]
+    values = ["^Oracle-Linux-8([\\.0-9\\-\\0-9\\.]+)$"]
     regex  = true
   }
 }
+
 data "oci_core_images" "centos_7" {
   compartment_id           = local.compartment_id
   operating_system         = "CentOS"
