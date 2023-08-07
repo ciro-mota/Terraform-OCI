6fc92242f6b06a45ca6e0cc985f51f2862b69805
Ciro Mota <ciro.mota@gmail.com>
2023-08-07 17:44:10 -0300

    fix: Added Oracle Linux 9 and Ubuntu 22.04 aarch64 and translated comments

--- instance.tf
+++ instance.tf
@@ -9,10 +9,11 @@ resource "oci_core_instance" "instance" {
   compartment_id      = local.compartment_id
   count               = var.count_instance
   display_name        = var.instance_name
-  shape               = var.shape_amd # var.shape_arm para usar o shape ARM. Configue no arquivo de variáveis o tamanho de CPU e MEM.
+  shape               = var.shape_amd
+  #  shape               = var.shape_arm # To use with ARM shape. See variable.tf file to configue CPU and MEM sizes.
 
   # shape_config {                            #
-  #    ocpus         = var.shape_arm_cpus    # Descomente este bloco ao uso com o shape ARM.
+  #    ocpus         = var.shape_arm_cpus    # Uncomment this block code to use with ARM shape.
   #    memory_in_gbs = var.shape_arm_memory #
   #  }
 
@@ -26,14 +27,16 @@ resource "oci_core_instance" "instance" {
   source_details {
     source_type = "image"
     source_id   = data.oci_core_images.ubuntu22.images.0.id
-    # source_id = data.oci_core_images.ubuntu_aarch64.images.0.id # Comente a entrada acima e descomente esta para usar a imagem do Ubuntu aarch64.
-    # source_id   = data.oci_core_images.oraclelinux.images.0.id # Comente a entrada acima e descomente esta para usar a imagem do Oracle Linux 8.x.
-    # source_id   = data.oci_core_images.centos_7.images.0.id # Comente a entrada acima e descomente esta para usar a imagem do CentOS 7.
+    # source_id = data.oci_core_images.ubuntu20.images.0.id # Comment above line and uncomment this one to use image from Ubuntu 20.04.
+    # source_id = data.oci_core_images.ubuntu22_aarch64.images.0.id # Comment above line and uncomment this one to use image from Ubuntu 22.04 aarch64.
+    # source_id = data.oci_core_images.ubuntu20_aarch64.images.0.id # Comment above line and uncomment this one to use image from Ubuntu 20.04 aarch64.
+    # source_id   = data.oci_core_images.oraclelinux8.images.0.id # Comment above line and uncomment this one to use image from Oracle Linux 8.x.
+    # source_id   = data.oci_core_images.oraclelinux9.images.0.id # Comment above line and uncomment this one to use image from Oracle Linux 9.x.
   }
 
   metadata = {
     ssh_authorized_keys = var.public_key_path
-    # user_data           = "${base64encode(file("script.sh"))}" # Descomente esta entrada para usar um script de implantação da instância.
+    # user_data           = "${base64encode(file("script.sh"))}" # Uncomment this line to use an instance deployment script. View and adapt the contents of the attached script.sh file.
   }
 
 }
