933af04e36e522846d64551a4e3e99460d87de3d
Ciro Mota <ciro.mota@gmail.com>
2023-12-15 17:15:29 -0300

    refactor: Added new verification and ansible steps

--- scripts/script.sh
+++ scripts/script.sh
@@ -3,57 +3,49 @@
 # Modify this script as needed to apply your deployment as soon as the instance starts.
 #
 
-# Function with commands to be used in Ubuntu.
-ubuntu() {
-
-	# Export the variable to not ask for confirmation during update operations.
+if [ -f /etc/debian_version ]; then
 	export DEBIAN_FRONTEND=noninteractive
+	sudo apt-get -q update && sudo apt-get -qy install ansible
 
-	echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-	echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-	sudo apt update
-	sudo apt upgrade -y
-	sudo apt install nginx -y
-	echo -e "<h1>It Works</h1>" | sudo tee /var/www/html/index*.html
-	sudo iptables -D INPUT -j REJECT --reject-with icmp-host-prohibited && sudo netfilter-persistent save
-}
-
-# Function with commands to be used in Oracle Linux.
-oraclelinux() {
-
-	inst() {
-		sudo dnf update -y
-		sudo dnf install -y nginx
+	firewallDebRule() {
+		echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+		echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+		sudo iptables -D INPUT -j REJECT --reject-with icmp-host-prohibited && sudo netfilter-persistent save
 	}
 
-	firewallrule() {
-		sudo firewall-cmd --permanent --zone=public --add-service=http
-		sudo firewall-cmd --permanent --zone=public --add-port=80/tcp
-		sudo firewall-cmd --permanent --zone=public --add-port=443/tcp
-		sudo firewall-cmd --reload
+	firewallDebRule
+
+elif [ -f /etc/redhat-release ]; then
+	sudo dnf install -y ansible-core
+
+	firewallRhelRule() {
+		firewall-cmd --permanent --zone=public --add-port=80/tcp &&
+		firewall-cmd --permanent --zone=public --add-port=443/tcp &&
+		firewall-cmd --reload
 	}
 
-	mods() {
-		sudo groupadd www
-		sudo adduser -G nginx -g www -d /usr/share/nginx/html www --system --shell=/bin/false
-		sudo echo -e "<h1>It Works</h1>" | sudo tee /usr/share/nginx/html/index.html
-	}
+	firewallRhelRule
 
-	servs() {
-		sudo systemctl enable --now nginx
-		sudo systemctl start nginx
-	}
-
-	inst
-	firewallrule
-	mods
-	servs
-}
-
-# Checks the systems and runs the respective script.
-if [[ $(lsb_release -is) = 'Ubuntu' ]] </dev/null >/dev/null 2>&1; then
-	ubuntu
+	ansible-galaxy collection install community.general
 else
-	[[ $(cat /etc/*release | head -1 | awk '{ print $1 }') =~ ^(Oracle|AlmaLinux)$ ]] </dev/null >/dev/null 2>&1
-	oraclelinux
+	echo "Unsuported Distro."
 fi
+
+tee -a requirements.yml <<'EOF'
+---
+- src: nginxinc.nginx
+EOF
+
+tee -a playbook.yml <<'EOF'
+---
+- name: "Provision Nginx"
+  hosts: localhost
+  become: true
+  roles:
+    - nginxinc.nginx
+EOF
+
+ansible-galaxy install -r requirements.yml
+ansible-playbook playbook.yml
+
+printf "<h1>Oracle OCI - It Works</h1>" >/usr/share/nginx/html/index.html
