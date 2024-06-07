c370a2fd68f7436076e882827f5da9e98367ce72
Ciro Mota <ciro.mota@gmail.com>
2024-06-07 17:45:40 -0300

    refactor: Little refactor and changes in playbook.yml

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # Modify this script as needed to apply your deployment as soon as the instance starts.
 #
 
 if [ -f /etc/debian_version ]; then
-	export DEBIAN_FRONTEND=noninteractive
-	sudo apt-get -q update && sudo apt-get -qy install ansible
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get -q update && apt-get -qy install ansible
 
-	firewallDebRule() {
+  firewallDebRule() {
 		echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 		echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 		sudo iptables -D INPUT -j REJECT --reject-with icmp-host-prohibited && sudo netfilter-persistent save
@@ -16,36 +16,64 @@ if [ -f /etc/debian_version ]; then
 	firewallDebRule
 
 elif [ -f /etc/redhat-release ]; then
-	sudo dnf install -y ansible-core
+  dnf install -y ansible-core firewalld
 
-	firewallRhelRule() {
-		firewall-cmd --permanent --zone=public --add-port=80/tcp &&
-		firewall-cmd --permanent --zone=public --add-port=443/tcp &&
-		firewall-cmd --reload
-	}
+  firewallrule() {
+    systemctl start firewalld && \
+    systemctl enable firewalld && \
+    firewall-cmd --permanent --zone=public --add-port=80/tcp && \
+    firewall-cmd --permanent --zone=public --add-port=443/tcp && \
+    firewall-cmd --reload
+    }
 
-	firewallRhelRule
-
-	ansible-galaxy collection install community.general
+  firewallrule
+  ansible-galaxy collection install community.general
 else
-	echo "Unsuported Distro."
+  echo "Unsuported Distro."
 fi
 
 tee -a requirements.yml <<'EOF'
 ---
-- src: nginxinc.nginx
+- src: dev-sec.ssh-hardening
+- src: geerlingguy.nginx
 EOF
 
 tee -a playbook.yml <<'EOF'
 ---
-- name: "Provision Nginx"
-  hosts: localhost
+- hosts: localhost
   become: true
-  roles:
-    - nginxinc.nginx
+  vars:
+    ssh_kex:
+    - sntrup761x25519-sha512@openssh.com
+    - curve25519-sha256@libssh.org
+    - diffie-hellman-group-exchange-sha256
+    ssh_server_ports: ['22']
+    ssh_permit_root_login: "without-password"
+    ssh_use_pam: "true"
+    sshd_authenticationmethods: "publickey"
+    ssh_authorized_keys_file: ".ssh/authorized_keys"
+  become: true
+  tasks:
+    - name: Install Nginx
+      include_role:
+        name: geerlingguy.nginx
+    - name: SSH Hardening
+      include_role:
+        name: dev-sec.ssh-hardening
+  handlers:
+    - name: start nginx
+      service:
+        name: nginx
+        state: started
 EOF
 
 ansible-galaxy install -r requirements.yml
 ansible-playbook playbook.yml
 
-printf "<h1>Oracle OCI - It Works</h1>" >/usr/share/nginx/html/index.html
+if [ -f /etc/debian_version ]; then
+  printf "<h1>DigitalOcean - It Works</h1>" > /var/www/html/index.nginx-debian.html
+elif [ -f /etc/redhat-release ]; then
+  printf "<h1>DigitalOcean - It Works</h1>" > /usr/share/nginx/html/index.html
+else
+  echo "Unsuported Distro."
+fi
