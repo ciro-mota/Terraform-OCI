580a99be49e6d9155f6e9d884d1f9f6598a26f13
Ciro Mota <ciro.mota@gmail.com>
2024-06-07 17:39:50 -0300

    fix: Added and comment data for Ubuntu 24, Oracle Linux aarch and removed others

--- datasource.tf
+++ datasource.tf
@@ -1,27 +1,3 @@
-data "oci_core_images" "ubuntu20" {
-  compartment_id   = local.compartment_id
-  operating_system = "Canonical Ubuntu"
-  sort_by          = "TIMECREATED"
-
-  filter {
-    name   = "display_name"
-    values = ["^Canonical-Ubuntu-20.04-([\\.0-9-]+)$"]
-    regex  = true
-  }
-}
-
-data "oci_core_images" "ubuntu22" {
-  compartment_id   = local.compartment_id
-  operating_system = "Canonical Ubuntu"
-  sort_by          = "TIMECREATED"
-
-  filter {
-    name   = "display_name"
-    values = ["^Canonical-Ubuntu-22.04-([\\.0-9-]+)$"]
-    regex  = true
-  }
-}
-
 data "oci_core_images" "ubuntu20_aarch64" {
   compartment_id   = local.compartment_id
   operating_system = "Canonical Ubuntu"
@@ -46,26 +22,26 @@ data "oci_core_images" "ubuntu22_aarch64" {
   }
 }
 
-data "oci_core_images" "oraclelinux8" {
+# data "oci_core_images" "ubuntu24_aarch64" {
+#   compartment_id   = local.compartment_id
+#   operating_system = "Canonical Ubuntu"
+#   sort_by          = "TIMECREATED"
+
+#   filter {
+#     name   = "display_name"
+#     values = ["^Canonical-Ubuntu-24.04-aarch64-([\\.0-9-]+)$"]
+#     regex  = true
+#   }
+# }
+
+data "oci_core_images" "oraclelinux9_aarch64" {
   compartment_id   = local.compartment_id
   operating_system = "Oracle Linux"
   sort_by          = "TIMECREATED"
 
   filter {
     name   = "display_name"
-    values = ["^Oracle-Linux-8([\\.0-9\\-\\0-9\\.]+)$"]
-    regex  = true
-  }
-}
-
-data "oci_core_images" "oraclelinux9" {
-  compartment_id   = local.compartment_id
-  operating_system = "Oracle Linux"
-  sort_by          = "TIMECREATED"
-
-  filter {
-    name   = "display_name"
-    values = ["^Oracle-Linux-9([\\.0-9\\-\\0-9\\.]+)$"]
+    values = ["^Oracle-Linux-9\\.[0-9]+-aarch64-[0-9]{4}\\.[0-9]{2}\\.[0-9]{2}-0$"]
     regex  = true
   }
 }
