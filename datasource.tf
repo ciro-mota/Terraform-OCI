1fcd920aeb1a923404f69bfd7e6fa2a301d0bf50
Ciro Mota <ciro.mota@gmail.com>
2025-02-25 15:57:08 -0300

    fix: Enable Ubuntu 24.04

--- datasource.tf
+++ datasource.tf
@@ -22,17 +22,17 @@ data "oci_core_images" "ubuntu22_aarch64" {
   }
 }
 
-# data "oci_core_images" "ubuntu24_aarch64" {
-#   compartment_id   = local.compartment_id
-#   operating_system = "Canonical Ubuntu"
-#   sort_by          = "TIMECREATED"
+data "oci_core_images" "ubuntu24_aarch64" {
+  compartment_id   = local.compartment_id
+  operating_system = "Canonical Ubuntu"
+  sort_by          = "TIMECREATED"
 
-#   filter {
-#     name   = "display_name"
-#     values = ["^Canonical-Ubuntu-24.04-aarch64-([\\.0-9-]+)$"]
-#     regex  = true
-#   }
-# }
+  filter {
+    name   = "display_name"
+    values = ["^Canonical-Ubuntu-24.04-aarch64-([\\.0-9-]+)$"]
+    regex  = true
+  }
+}
 
 data "oci_core_images" "oraclelinux9_aarch64" {
   compartment_id   = local.compartment_id
